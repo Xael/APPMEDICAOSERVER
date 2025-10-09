005a8b87f4c739d5b8c9c3d93edf064da9be0957
Xael <67170424+Xael@users.noreply.github.com>
2025-10-09 13:26:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const unitRoutes = require('./routes/units');
 const goalRoutes = require('./routes/goals');
 const auditLogRoutes = require('./routes/auditLog');
 const reportRoutes = require('./routes/reports.js');
-
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -26,7 +26,8 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // API Routes
